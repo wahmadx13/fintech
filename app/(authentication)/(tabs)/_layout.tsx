3f104b43681d5c0bfa9d80b76014e54cf0f11208
Waseem <wasim_ahmad2003@yahoo.com>
2024-07-15 17:37:04 +0500

    added necessary files for various tabs

---
+++ app/(authentication)/(tabs)/_layout.tsx
@@ -0,0 +1,12 @@
+import { Tabs } from "expo-router";
+import { View, Text } from "react-native";
+
+const Layout = () => {
+  return (
+    <Tabs>
+      <Text>Layout</Text>
+    </Tabs>
+  );
+};
+
+export default Layout;
