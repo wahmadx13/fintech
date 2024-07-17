38dcdc55a1d43a89ef5c658fddde35eaa1fb035f
Waseem <wasim_ahmad2003@yahoo.com>
2024-07-17 21:03:37 +0500

    added blur view to the tab bar

--- app/(authenticated)/(tabs)/_layout.tsx
+++ app/(authenticated)/(tabs)/_layout.tsx
@@ -1,5 +1,6 @@
 import Colors from "@/constants/Colors";
 import { FontAwesome } from "@expo/vector-icons";
+import { BlurView } from "expo-blur";
 import { Tabs } from "expo-router";
 
 const Layout = () => {
@@ -7,8 +8,23 @@ const Layout = () => {
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors.primary,
-        tabBarLabelStyle: {
-          fontSize: 12,
+        tabBarBackground: () => (
+          <BlurView
+            intensity={100}
+            style={{
+              flex: 1,
+              backgroundColor: "rgba(0,0,0,0.05)",
+            }}
+          />
+        ),
+        tabBarStyle: {
+          backgroundColor: "transparent",
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          elevation: 0,
+          borderTopWidth: 0,
         },
       }}
     >
