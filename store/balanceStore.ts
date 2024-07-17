d85b21f450740bf9e61a04019fd69ff16e69ea1d
Waseem <wasim_ahmad2003@yahoo.com>
2024-07-17 15:01:57 +0500

    renamed mmkv-storage.ts file to secure-storage.ts

--- store/balanceStore.ts
+++ store/balanceStore.ts
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
-import { zustandStorage } from "./mmkv-storage";
+import { zustandStorage } from "./secure-storage";
 
 export interface Transaction {
     id: string;
