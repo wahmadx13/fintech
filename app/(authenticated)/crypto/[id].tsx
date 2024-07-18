9427f5e8535c402ec73853b84c4a271a7d06b281
Waseem <wasim_ahmad2003@yahoo.com>
2024-07-18 18:22:08 +0500

    rendered dynami data on crypto id page

--- app/(authenticated)/crypto/[id].tsx
+++ app/(authenticated)/crypto/[id].tsx
@@ -1,14 +1,187 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { useLocalSearchParams } from "expo-router";
+import {
+  View,
+  Text,
+  SectionList,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import React, { useState } from "react";
+import { useHeaderHeight } from "@react-navigation/elements";
+import { Stack, useLocalSearchParams } from "expo-router";
+import { defaultStyles } from "@/constants/Styles";
+import Colors from "@/constants/Colors";
+import { useQuery } from "@tanstack/react-query";
+import { Ionicons } from "@expo/vector-icons";
+
+const categories = ["Overview ", "News", "Orders", "Transactions"];
 
 const Page = () => {
   const { id } = useLocalSearchParams();
+  const headerHeight = useHeaderHeight();
+
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+
+  const { data } = useQuery({
+    queryKey: ["info", id],
+    queryFn: async () => {
+      const info = await fetch(`/api/info?ids=${id}`).then((res) => res.json());
+      return info[+id!];
+    },
+  });
   return (
-    <View>
-      <Text>Details</Text>
-    </View>
+    <>
+      <Stack.Screen options={{ title: data?.name }} />
+      <SectionList
+        style={{ marginTop: headerHeight }}
+        contentInsetAdjustmentBehavior="automatic"
+        stickySectionHeadersEnabled={true}
+        keyExtractor={(i) => i.title}
+        sections={[{ data: [{ title: "Chart" }] }]}
+        renderSectionHeader={() => (
+          <ScrollView
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{
+              alignItems: "center",
+              justifyContent: "space-between",
+              width: "100%",
+              paddingHorizontal: 16,
+              paddingBottom: 8,
+              backgroundColor: Colors.background,
+              borderBottomColor: Colors.lightGray,
+              borderBottomWidth: StyleSheet.hairlineWidth,
+            }}
+          >
+            {categories.map((item, index) => (
+              <TouchableOpacity
+                key={index}
+                onPress={() => setActiveIndex(index)}
+                style={
+                  activeIndex === index
+                    ? styles.categoriesBtnActive
+                    : styles.categoriesBtn
+                }
+              >
+                <Text
+                  style={
+                    activeIndex === index
+                      ? styles.categoryTextActive
+                      : styles.categoryText
+                  }
+                >
+                  {item}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        )}
+        ListHeaderComponent={() => (
+          <>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+                marginHorizontal: 16,
+              }}
+            >
+              <Text style={styles.subtitle}>{data?.symbol}</Text>
+              <Image
+                source={{ uri: data?.logo }}
+                style={{ width: 60, height: 60 }}
+              />
+            </View>
+            <View style={{ flexDirection: "row", gap: 10, margin: 12 }}>
+              <TouchableOpacity
+                style={[
+                  defaultStyles.pillButtonSmall,
+                  {
+                    backgroundColor: Colors.primary,
+                    flexDirection: "row",
+                    gap: 16,
+                  },
+                ]}
+              >
+                <Ionicons name="add" size={24} color={"#fff"} />
+                <Text style={[defaultStyles.buttonText, { color: "#fff" }]}>
+                  Buy
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[
+                  defaultStyles.pillButtonSmall,
+                  {
+                    backgroundColor: Colors.primaryMuted,
+                    flexDirection: "row",
+                    gap: 16,
+                  },
+                ]}
+              >
+                <Ionicons name="arrow-back" size={24} color={Colors.primary} />
+                <Text
+                  style={[defaultStyles.buttonText, { color: Colors.primary }]}
+                >
+                  Receive
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </>
+        )}
+        renderItem={({ item }) => (
+          <>
+            {/*Chart Rendering*/}
+            <View style={{ height: 500 }}></View>
+            <View style={[defaultStyles.block, { marginTop: 20 }]}>
+              <Text style={styles.subtitle}>Overview</Text>
+              <Text style={{ color: Colors.gray }}>
+                Bitcoin is a decentralized digital currency, without a central
+                bank or single administrator, that can be sent from user to user
+                on the peer-to-peer bitcoin network without the need for
+                intermediaries. Transactions are verified by network nodes
+                through cryptography and recorded in a public distributed ledger
+                called a blockchain.
+              </Text>
+            </View>
+          </>
+        )}
+      ></SectionList>
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  subtitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 20,
+    color: Colors.gray,
+  },
+  categoryText: {
+    fontSize: 14,
+    color: Colors.gray,
+  },
+  categoryTextActive: {
+    fontSize: 14,
+    color: "#000",
+  },
+  categoriesBtn: {
+    padding: 10,
+    paddingHorizontal: 14,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 20,
+  },
+  categoriesBtnActive: {
+    padding: 10,
+    paddingHorizontal: 14,
+
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    borderRadius: 20,
+  },
+});
 
 export default Page;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,7 @@ import { Link, Stack, useRouter, useSegments } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
-import { TouchableOpacity, Text } from "react-native";
+import { TouchableOpacity, Text, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import * as SecureStore from "expo-secure-store";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -156,6 +156,33 @@ const InitialLayout = () => {
         name="(authenticated)/(tabs)"
         options={{
           headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="(authenticated)/crypto/[id]"
+        options={{
+          title: "",
+          headerLeft: () => (
+            <TouchableOpacity onPress={router.back}>
+              <Ionicons name="arrow-back" size={34} color={Colors.dark} />
+            </TouchableOpacity>
+          ),
+          headerLargeTitle: true,
+          headerTransparent: true,
+          headerRight: () => (
+            <View style={{ flexDirection: "row", gap: 10 }}>
+              <TouchableOpacity>
+                <Ionicons
+                  name="notifications-outline"
+                  color={Colors.dark}
+                  size={30}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Ionicons name="star-outline" color={Colors.dark} size={30} />
+              </TouchableOpacity>
+            </View>
+          ),
         }}
       />
     </Stack>
