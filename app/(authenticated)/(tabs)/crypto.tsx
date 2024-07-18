cbad8b503814210d15bbaca097635bdc2ec8ea3f
Waseem <wasim_ahmad2003@yahoo.com>
2024-07-18 16:22:45 +0500

    added crypto info and top performers apis

--- app/(authenticated)/(tabs)/crypto.tsx
+++ app/(authenticated)/(tabs)/crypto.tsx
@@ -1,11 +1,47 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Image } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import Colors from "@/constants/Colors";
+import { useHeaderHeight } from "@react-navigation/elements";
+import { useQuery } from "@tanstack/react-query";
+import { Currency } from "@/interfaces/crypto";
 
 const Page = () => {
+  const headerHeight = useHeaderHeight();
+
+  const currencies = useQuery({
+    queryKey: ["listings"],
+    queryFn: () => fetch("/api/listings").then((res) => res.json()),
+  });
+
+  const ids = currencies.data
+    ?.map((currency: Currency) => currency.id)
+    .join(", ");
+
+  const { data } = useQuery({
+    queryKey: ["info", ids],
+    queryFn: () => fetch(`/api/info?ids=${ids}`).then((res) => res.json()),
+    enabled: !!ids,
+  });
+
   return (
-    <View>
-      <Text>Crypto</Text>
-    </View>
+    <ScrollView
+      style={{ backgroundColor: Colors.background }}
+      contentContainerStyle={{
+        paddingTop: headerHeight,
+      }}
+    >
+      <View>
+        {currencies.data?.map((currency: Currency) => (
+          <View style={{ flexDirection: "row" }} key={currency.id}>
+            <Image
+              source={{ uri: data?.[currency.id].logo }}
+              style={{ width: 32, height: 32 }}
+            />
+            <Text>{currency.name}</Text>
+          </View>
+        ))}
+      </View>
+    </ScrollView>
   );
 };
 
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,9 +10,12 @@ import { useEffect } from "react";
 import { TouchableOpacity, Text } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import * as SecureStore from "expo-secure-store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "react-native-reanimated";
 
 const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+const queryClient = new QueryClient();
 
 // Cache the Clerk JWT
 
@@ -165,10 +168,12 @@ const RootLayoutNav = () => {
       publishableKey={CLERK_PUBLISHABLE_KEY!}
       tokenCache={tokenCache}
     >
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <StatusBar style="light" />
-        <InitialLayout />
-      </GestureHandlerRootView>
+      <QueryClientProvider client={queryClient}>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <StatusBar style="light" />
+          <InitialLayout />
+        </GestureHandlerRootView>
+      </QueryClientProvider>
     </ClerkProvider>
   );
 };
